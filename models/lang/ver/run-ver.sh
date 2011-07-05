2609aafb65a053c2e6fe8a0210557c246108ceae
Maciek Pazurkiewicz <mp248267@students.mimuw.edu.pl>
2011-07-05 15:24:01 +0200

    Updated verification utils

--- models/lang/ver/run-ver.sh
+++ models/lang/ver/run-ver.sh
@@ -3,35 +3,33 @@
 VER='/opt/dev-uppaal/bin-Linux/verifyta'
 DEFOPTS='-s -u'
 
-if [ $# -lt 2 ]
+if [ $# -lt 4 ]
 then
     echo "Script should be run with at least two arguments"
     exit 1
 fi
 
-model=$1
+model_file=$1
 shift
-num=$1
+prop_file=$1
 shift
-
-model_file="$model.xml"
-verdir="$model.ver"
-propfile="$verdir/$num.q"
-verout="$verdir/$num.ver"
-statout="$verdir/$num.stats"
+ver_out=$1
+shift
+stat_out=$1
+shift
 
 timestart=`date +%s`
-$VER $DEFOPTS $model_file $propfile $@ >$verout &
+$VER $DEFOPTS $model_file $prop_file $@ >$ver_out &
 verpid=$!
-top -b -d1.00 -p $verpid >$statout &
+top -b -d1.00 -p $verpid >$stat_out &
 toppid=$!
 
 wait $verpid
 timefin=`date +%s`
 kill -2 $toppid
 
-echo "$timestart $timefin verifyta" >> $statout
+echo "$timestart $timefin verifyta" >> $stat_out
 
-grep "verifyta" $statout | tail -n 2 > "$statout.tmp"
-mv "$statout.tmp" $statout
+grep "verifyta" $stat_out | tail -n 2 > "$stat_out.tmp"
+mv "$stat_out.tmp" $stat_out
 
